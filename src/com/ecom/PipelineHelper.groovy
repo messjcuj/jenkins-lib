e6d2f0d90b6739b8dfa72fe5120fb55d0a8b74c9
Your Name <tuanphuc2k@gmail.com>
2025-07-12 02:58:45 +0700

    asdas

--- src/com/ecom/PipelineHelper.groovy
+++ src/com/ecom/PipelineHelper.groovy
@@ -4,13 +4,13 @@ import groovy.json.JsonSlurper
 
 class PipelineHelper implements Serializable {
 
-    static void getDefaultAgents() {
+    static void getDefaultAgents(def script) {
         def jsonStream = script.libraryResource("settings.json")
         def json = new JsonSlurper().parseText(jsonStream)
         return json["default_agent_label"]
     }
 
-    static void getAgents() {
+    static void getAgents(def script) {
         def jsonStream = script.libraryResource("settings.json")
         def json = new JsonSlurper().parseText(jsonStream)
         return json["agent_label"]
