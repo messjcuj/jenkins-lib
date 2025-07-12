7bff99698fb27a1d21ecfb2a2a516bdf0dae2456
Your Name <tuanphuc2k@gmail.com>
2025-07-12 21:39:00 +0700

    asda

--- src/com/ecom/PipelineGlobalConfig.groovy
+++ src/com/ecom/PipelineGlobalConfig.groovy
@@ -12,5 +12,16 @@ class PipelineGlobalConfig implements Serializable {
     def isSubmodules = false
     def rootModuleRepo = ""
     def submodulesRepos = []
+    def defaultAgentLabel = "default-agent"
+    def agentLabels = [
+        "agent-1",
+        "agent-2"
+    ]
+    def jdkHome = [
+        "21": "/opt/java/jdk-21.0.7"
+    ]
+    def gradleHome = [
+        "8": "/opt/gradle/gradle-8.14.3"
+    ]
 
 }
