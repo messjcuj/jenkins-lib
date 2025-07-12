c667b0205be014424232c546cf7d83a8456ac93f
Your Name <tuanphuc2k@gmail.com>
2025-07-12 20:43:40 +0700

    asda

--- vars/GradleBuild.groovy
+++ vars/GradleBuild.groovy
@@ -34,7 +34,7 @@ def call(Map parameters = [:]) {
 
     for (repo in globalConfig.repos) {
         parallelSteps[repo] = {
-            def command = "gradle clean build -Dorg.gradle.java.home=${javaHome} ${parameters.global_arguments}"
+            def command = "${gradleHome}/gradle clean build -Dorg.gradle.java.home=${javaHome} ${parameters.global_arguments}"
             buildCommand(repo, command)
         }
     }
