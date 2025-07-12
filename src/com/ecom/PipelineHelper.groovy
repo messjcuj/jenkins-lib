325cea39885e2a988ae87deb1ffde78994c29fdb
Your Name <tuanphuc2k@gmail.com>
2025-07-12 21:33:44 +0700

    asda

--- src/com/ecom/PipelineHelper.groovy
+++ src/com/ecom/PipelineHelper.groovy
@@ -2,7 +2,7 @@ package com.ecom
 
 import groovy.json.JsonSlurper
 
-class PipelineHelper implements Serializable {
+class PipelineHelper {
 
     static void getSettings(def script) {
         def jsonStream = script.libraryResource("settings.json")
